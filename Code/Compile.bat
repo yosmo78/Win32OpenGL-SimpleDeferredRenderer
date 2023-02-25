69f92dbdbb2d68abdffd1747ab6820f48a9a4749
yosmo78 <yosmo78@gmail.com>
2023-02-25 14:36:46 -0800

    Added Support for visual studio 2022

--- Code/Compile.bat
+++ Code/Compile.bat
@@ -1,5 +1,8 @@
 @echo off
 if not defined DevEnvDir (
+	call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+if not defined DevEnvDir (
 	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 )
 
